34cb6edd28b2c4a3863c059535ef54293d26c695
spalme21 <spalme21@gmail.com>
2023-07-26 14:08:44 -0500

    Create UI

--- scripts.js
+++ scripts.js
@@ -1,11 +1,20 @@
+const buttons = document.querySelectorAll('button');
+
+buttons.forEach((button) => {
+    button.addEventListener('click', playRound);
+});
+
+const results = document.querySelector("#results");
+
+let playerScore = 0;
+let computerScore = 0;
+
 function getComputerChoice() {
     const choices = ["rock", "paper", "scissors"];
     return choices[Math.floor(Math.random() * 3)];
-}
+};
 
-function playRound() {
-    const playerSelection = prompt("Make your choice. Enter 'Rock, Paper, or Scissors: ").toLowerCase();
-    const computerSelection = getComputerChoice();
+function getRoundResult(playerSelection, computerSelection) {
     if (playerSelection == "rock") {
         if (computerSelection == "rock") {
             return "Draw!";
@@ -31,13 +40,60 @@ function playRound() {
             return "Draw!";
         }
     }
+};
+
+function updateScore(result) {
+    if (result === "You win!") {
+        playerScore++;
+    } else if (result === "You lose!") {
+        computerScore++;
+    }
+};
+
+function displayScore() {
+    const playerSpan = document.getElementById("player-score");
+    const computerSpan = document.getElementById("computer-score");
+    playerSpan.textContent = playerScore;
+    computerSpan.textContent = computerScore;
+};
+
+function checkForWinner() {
+    if (playerScore == 5) {
+        const win = document.createElement('h2');
+        win.textContent = "You win!"
+        results.insertBefore(win, results.firstChild);
+    } else if (computerScore == 5) {
+        const lose = document.createElement('h2');
+        lose.textContent = "You lose!"
+        results.insertBefore(lose, results.firstChild);
+    }
 }
 
-function displayScore(playerScore, computerScore) {
-    console.log(`Your score: ${playerScore}`);
-    console.log(`My score: ${computerScore}`);
-}
+function playRound(e) {
+    const round = document.createElement('div')
 
+    const playerSelection = e.target.textContent.toLowerCase();
+    const pPlayerSelection = document.createElement('p');
+    pPlayerSelection.textContent = `You chose ${playerSelection}`;
+    round.appendChild(pPlayerSelection);
+
+    const computerSelection = getComputerChoice();
+    const pComputerSelection = document.createElement('p');
+    pComputerSelection.textContent = `I choose ${computerSelection}`;
+    round.appendChild(pComputerSelection);
+
+    const p = document.createElement('p');
+    p.textContent = getRoundResult(playerSelection, computerSelection);
+    round.appendChild(p);
+
+    results.insertBefore(round, results.firstChild);
+
+    updateScore(p.textContent);
+    displayScore();
+    checkForWinner();
+};
+
+/*
 function game() {
     console.log("Let's play Rock Paper Scissors");
     let playerScore = 0;
@@ -60,3 +116,5 @@ function game() {
         console.log("Draw!");
     }
 }
+*/
+
